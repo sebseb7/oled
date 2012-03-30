556e4c42725a015f3506599e3d73a2c822583009
sebseb7 <github@seb.exse.net>
2012-03-31 00:24:50 +0200

    red green fader

---
+++ apps/red_green_fader.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+
+#include "wuline.h"
+#include "circle.h"
+#include "text.h"
+
+
+uint8_t a = 0;
+int8_t b = 1;
+
+
+
+static uint8_t tick(void) {
+
+	draw_filledCircle(64,64,70,0,0,0);
+	draw_filledCircle(64,64,62,a,255-a,0);
+
+	a+=b;
+	if(a==255)
+		b = -1;
+	if(a==0)
+		b = 1;
+
+	return 0;
+}
+
+
+
+static void init(void) ATTRIBUTES
+void init(void) {
+	
+	registerAnimation(tick, 10, 450);
+}
+
+
