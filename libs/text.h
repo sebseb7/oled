3fae8309793c52791b0c6fa6b4a25bab1c0b3806
sebseb7 <github@seb.exse.net>
2012-04-03 06:32:57 +0200

    added draw_number

--- libs/text.h
+++ libs/text.h
@@ -2,5 +2,6 @@
 #define _TEXT_H
 
 void draw_text(uint8_t x, uint8_t y, const char *text, uint8_t r,uint8_t g,uint8_t b);
+void draw_number(uint8_t x, uint8_t y, uint32_t number, uint8_t length, uint8_t pad, uint8_t r, uint8_t g , uint8_t b);
 
 #endif
